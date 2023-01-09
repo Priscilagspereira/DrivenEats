8d47628ee2c0b7a58e9ec09d09d03805f2c90950
priscilagspereira <priscilagabsantos@gmail.com>
2023-01-09 15:10:25 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let pratoSelecionado
+let bebidaSelecionada
+let sobremesaSelecionada
+let total=0
+
+function Selecionado(comida) {
+    if (pratoSelecionado != null) {
+        pratoSelecionado.querySelector.remove(.prato)
+
+    }
+}
+
+function teste() {
+    alert('oi')
+}
